d5cdf889ab43437eede83ec5d0956925486c6339
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-02-24 22:35:15 -0500

    Initial commit

---
+++ Auth.Example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonoGSoCIdeasPage
+{
+	// COPY THIS TO Auth.DONOTCOMMIT.cs and put your user token and app key IN THE COPY
+	// DO NOT COMMIT YOUR KEYS TO GIT
+	// EVER
+	// I REALLY MEAN IT
+	//
+	// Get your App key at https://trello.com/app-key
+	// Visit https://trello.com/1/authorize?key=APPKEY&name=MonoGSoCIdeasPage&expiration=never&response_type=token
+	// (where APPKEY is your app key) to get your user token
+	//
+	public class Auth
+	{
+		public static string UserToken = "xxxx";
+		public static string AppKey = "xxxx";
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Manatee.Trello.ManateeJson;
+using Manatee.Trello;
+using Manatee.Trello.RestSharp;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MonoGSoCIdeasPage
+{
+	class MainClass
+	{
+		// 
+		// to get your user token
+		// 
+		public static void Main (string[] args)
+		{
+			var serializer = new ManateeSerializer();
+			TrelloConfiguration.Serializer = serializer;
+			TrelloConfiguration.Deserializer = serializer;
+			TrelloConfiguration.JsonFactory = new ManateeFactory();
+			TrelloConfiguration.RestClientProvider = new RestSharpClientProvider();
+			TrelloAuthorization.Default.AppKey = Auth.AppKey;
+			TrelloAuthorization.Default.UserToken = Auth.UserToken;
+
+
+			var board = new Board("tj9zKDV4");
+			var list = board.Lists.Single (l => l.Name == "Ready");
+			var readyCards = list.Cards;
+
+			var page = new Page ();
+
+			page.Ideas = readyCards.Select (c => new Idea {
+				Area = c.Labels.Single (l => l.Color != LabelColor.Orange).Name,
+				Difficulty = c.Labels.Single (l => l.Color == LabelColor.Orange).Name.Split (' ').Last (),
+				Title = c.Name,
+				Mentors = string.Join (", ", c.Members.Select (m => m.FullName)),
+				Description = c.Description
+			}).GroupBy (i => i.Area).ToDictionary (g => g.Key, g => (IEnumerable<Idea>)g);
+
+			Console.WriteLine (page.TransformText ());
+		}
+	}
+
+	partial class Page
+	{
+		public IDictionary<string,IEnumerable<Idea>> Ideas;
+
+		void WriteIdeas (string area)
+		{
+			IEnumerable<Idea> areaIdeas;
+			if (Ideas.TryGetValue (area, out areaIdeas)) {
+				foreach (var idea in areaIdeas) {
+					WriteLine (idea.TransformText ());
+					WriteLine ("");
+				}
+			} else {
+				WriteLine ("**We don't have any ideas in this area right now, but feel free to propose your own!**");
+			}
+		}
+
+		const string MonoDotNetIntegration = "Mono .NET Integration";
+		const string MonoRuntime = "Mono Runtime";
+		const string CompilersAndTools = "Compilers and Tools";
+		const string MonoDevelop = "MonoDevelop";
+		const string GtkSharpAndBindings = "Gtk# and Bindings";
+	}
+
+	partial class Idea
+	{
+		public string Title;
+		public string Area;
+		public string Difficulty;
+		public string Mentors;
+		public string Description;
+	}
+}
